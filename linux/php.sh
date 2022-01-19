aeb2128e7f3f43dadc46866d7cf63b1e39c15316
jth55 <jthince55@gmail.com>
2022-01-19 06:41:03 +0000

    more linux scripts

--- linux/php.sh
+++ linux/php.sh
@@ -1,2 +1,24 @@
-# same as ssh but for php
+#!/bin/bash
 
+# find path of php.ini
+if [ -f /etc/php.ini ]; then
+	ini=/etc/php.ini
+elif [ -f /etc/apache2/php.ini ]; then
+	ini=/etc/php.ini
+else
+	echo "[*] default php.ini path not found. I saved config options in /tmp/php.ini. Copy this file to one of the following locations: "
+	ini=/tmp/php.ini
+	find / -name "php.ini" 2>/dev/null
+fi
+
+echo "disable_functions = shell_exec, exec, passthru, proc_open, popen, system, phpinfo" >> $ini
+echo "max_execution_time = 3" >> $ini
+echo "register_globals = off" >> $ini
+echo "magic_quotes_gpc = on" >> $ini 
+echo "allow_url_fopen = off" >> $ini 
+echo "allow_url_include = off" >> $ini 
+echo "display_errors = off" >> $ini 
+echo "short_open_tag = off" >> $ini 
+echo "session.cookie_httponly = 1" >> $ini 
+echo "session.use_only_cookies = 1" >> $ini 
+echo "session.cookie_secure = 1" >> $ini 
--- linux/restrict.sh
+++ linux/restrict.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# backup /etc/passwd just in case
+cp /etc/passwd /etc/passwd.bak
+chmod 644 /etc/passwd.bak
+
 # set rbash for non-root users
+head -1 /etc/passwd > /etc/pw
+sed -n '1!p' /etc/passwd | sed 's/\/bin\/bash/\/bin\/rbash/g' >> /etc/pw
+
+mv /etc/pw /etc/passwd
