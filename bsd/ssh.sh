f2d6445d987862b6404697898411a606044bfbaf
jth55 <jthince55@gmail.com>
2022-01-20 22:48:47 -0600

    Finished bsd and solaris

--- bsd/ssh.sh
+++ bsd/ssh.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# CHECK IF SSH IS RUNNING, if so, print message
-if [ $(systemctl is-active ssh | grep -vw "active" 2>/dev/null) ]
-then
-	echo "SSH is DOWN"
-fi
 
-# secure ssh
-sed -i '1s;^;PermitRootLogin No\n;' /etc/ssh/sshd_config
-sed -i '1s;^;PubkeyAuthentication No\n;' /etc/ssh/sshd_config
-sed -i '1s;^;UsePam No\n;' /etc/ssh/sshd_config
-sed -i '1s;^;UseDns No\n;' /etc/ssh/sshd_config
-sed -i '1s;^;AddressFamily inet\n;' /etc/ssh/sshd_config
+# secure sshd_config
+echo "PubkeyAuthentication no" > ssh.txt
+echo "PermitEmptyPasswords no" >> ssh.txt
+echo "UseDns no" >> ssh.txt
+echo "AddressFamily inet" >> ssh.txt
+
+cat /etc/ssh/sshd_config >> ssh.txt
+mv ssh.txt /etc/ssh/sshd_config
 
 # restart
-systemctl restart ssh
+sshd -t && service sshd restart
--- solaris/users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-users=`getent passwd | cut -d ":" -f 1`m                                                                                                      
-for user in $users; do                 
-    pass=`dd if=/dev/urandom count=4 bs=1 | digest -a md5 | cut -c -10`          
-    hash=`/usr/sfw/bin/openssl passwd -1 "$pass"`                                                                                                                            
-    echo "$user:$hash:::::::" >> /etc/shadow.bak  
-    echo "$user,$pass"
-done
-mv /etc/shadow.bak /etc/shadow
