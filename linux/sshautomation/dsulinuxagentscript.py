b6c8ca38dfe7042168c45e94113edb7bca8ddf95
BraydenPikachu <braydenpikachuyt@gmail.com>
2023-12-05 03:04:04 -0800

    Adds logging to each module, added php module,

--- linux/sshautomation/dsulinuxagentscript.py
+++ linux/sshautomation/dsulinuxagentscript.py
@@ -2,18 +2,85 @@ import os
 import paramiko
 import yaml
 
-def Root_Password_Changes(ssh, root_password):
-    ssh.exec_command(f'passwd root {root_password}')
+from datetime import datetime
 
-def User_Password_Changes(ssh, user_password):
-    ssh.exec_command(f'for u in $(cat /etc/passwd|grep -v ^root:|cut -d: -f1);do echo "$u:{user_password}"|chpasswd;done')
+def log_command_execution(command, result, box):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open('Command_Log.txt', 'a') as log_file:
+        log_file.write(f"[{timestamp}] Box: {box}\n")
+        log_file.write(f"Command: {command}\n")
+        log_file.write(f"Result:\n{result}\n")
+        log_file.write("=" * 40 + "\n\n")
 
-def files_to_backup(ssh, files_to_backup):
+def Root_Password_Changes(ssh, root_password, box):
+    command = f'passwd root {root_password}'
+    _, stdout, _ = ssh.exec_command(command)
+    result = stdout.read().decode('utf-8').strip()
+    log_command_execution(command, result, box)
+
+def User_Password_Changes(ssh, user_password, box):
+    command = f'for u in $(cat /etc/passwd | grep -v ^root: | cut -d: -f1); do echo "$u:{user_password}" | chpasswd; done'
+    _, stdout, _ = ssh.exec_command(command)
+    result = stdout.read().decode('utf-8').strip()
+    log_command_execution(command, result, box)
+
+def files_to_backup(ssh, files_to_backup, box):
     for file in files_to_backup:
-        ssh.exec_command(f'cp {file} /root/{file}')
-def firewall_stuff(ssh, ports):
-    #Implement this here lol
-    pass
+        command = f'cp {file} /root/{file}'
+        _, stdout, _ = ssh.exec_command(command)
+        result = stdout.read().decode('utf-8').strip()
+        log_command_execution(command, result, box)
+
+def firewall_stuff(ssh, ports, box):
+    # Implement this here
+    command = "Your Firewall Command Here"
+    _, stdout, _ = ssh.exec_command(command)
+    result = stdout.read().decode('utf-8').strip()
+    log_command_execution(command, result, box)
+
+def audit_Users(ssh, box):
+    _, stdout, _ = ssh.exec_command("grep -E '/bash$|/sh$' /etc/passwd")
+    users_data = stdout.read().decode('utf-8').strip()
+    section_header = f"=== {box} ==="
+    data_to_write = f"{section_header}\n{users_data}\n\n"
+    with open('Audited_Users.txt', 'a') as file:
+        file.write(data_to_write)
+    log_command_execution("Audit Users", users_data, box)
+
+def execute_BashScript(ssh, script, box):
+    with open(script, 'r') as bash_script:
+        for line in bash_script:
+            _, stdout, _ = ssh.exec_command(line)
+            result = stdout.read().decode('utf-8').strip()
+            log_command_execution(line, result, box)
+def change_SSH_Settings(ssh,box):
+    #Maybe move to yml undecided atm
+    ssh_commands = [
+    "sed -i '1s;^;PermitRootLogin yes\n;' /etc/ssh/sshd_config",
+    "sed -i '1s;^;PubkeyAuthentication no\n;' /etc/ssh/sshd_config",
+    "sed -i '1s;^;UseDNS no\n;' /etc/ssh/sshd_config",
+	"sed -i '1s;^;PermitEmptyPasswords no\n;' /etc/ssh/sshd_config",
+	"sed -i '1s;^;AddressFamily inet\n;' /etc/ssh/sshd_config"
+    # Add more commands as needed
+]
+    for command in ssh_commands:
+        _, stdout, _ = ssh.exec_command(line)
+        result = stdout.read().decode('utf-8').strip()
+        log_command_execution(line, result, box)
+
+def modify_php_settings(ssh, php_config_path, settings, box):
+    for setting, value in settings.items():
+        command = f"echo '{setting} = {value}' >> {php_config_path}"
+        _, stdout, _ = ssh.exec_command(command)
+        result = stdout.read().decode('utf-8').strip()
+        log_command_execution(command, result, box)
+
+
+def run_single_command(ssh,box,cmd):
+    _, stdout, _ = ssh.exec_command(cmd)
+    result = stdout.read().decode('utf-8').strip()
+    log_command_execution(cmd, result, box)
+
 
 
 
@@ -26,7 +93,7 @@ with open(config_path, 'r') as file:
 # Extract the files_to_backup entry
 enabled_modules = data.get('enabled_modules', [])
 files_to_backup_entry = data.get('files_to_backup', [])
-
+php_settings_entry = data.get('php_settings', {})
 # Print the resulting files_to_backup entry
 print(files_to_backup_entry)
 with open("boxes.conf","r") as boxes:
