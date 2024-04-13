9358c41b886213d46f044ab550fda001eeee4724
dsiemienas03 <david@siemienas.dev>
2024-04-13 17:26:12 +0000

    Add firewall setup script and encrypt inventory file with ansible-vault

--- new-fw/palo/fw-setup.sh
+++ new-fw/palo/fw-setup.sh
@@ -8,4 +8,18 @@ ansible-galaxy collection install paloaltonetworks.panos
 pip install -r requirements.txt
 
 # Needs to be modified to run in script
+    # ask for password
 curl -H "Content-Type: application/x-www-form-urlencoded" -X POST https://firewall/api/?type=keygen -d 'user=<user>&password=<password>'
+
+
+# output to fw.yml file for invintory and encrypt with ansible-vault for another password
+echo "all:
+  hosts:
+    firewall:
+      hosts: <firewall>
+      ip_address: <password>
+      api_key: <api_key>
+      ansible_python_interpreter: /usr/bin/python3" > fw.yml
+
+# encrypt vault
+ansible-vault encrypt fw.yml
