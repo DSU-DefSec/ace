953acbf464504dcfe04da78cc0d51a4d33ced177
David Siemienas <david@siemienas.dev>
2025-04-08 17:09:34 +0000

    Updates for splunk and vm-setup script

--- vm-init/vm_setup.bash
+++ vm-init/vm_setup.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+HTTP_PROXY="https://10.120.0.200:8080"
+HTTPS_PROXY="https://10.120.0.200:8080"
+
+cp proxy.crt /etc/docker/certs.d/registry-1.docker.io/ca.crt
+
 # Update and upgrade the system
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -30,9 +35,21 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Configure Docker to use a proxy
+echo '{
+  "proxies": {
+    "http-proxy": "https://10.120.0.200:8080",
+    "https-proxy": "https://10.120.0.200:8080",
+    "no-proxy": "*.test.example.com,.example.org,127.0.0.0/8"
+  }
+}' | sudo tee /etc/docker/daemon.json > /dev/null
+
+# Restart Docker to apply the changes
+sudo systemctl restart docker
+
 # Install Splunk
 #
-wget -O splunk.deb "https://download.splunk.com/products/splunk/releases/9.4.1/linux/splunk-9.4.1-e3bdab203ac8-linux-amd64.deb"
-sudo dpkg -i splunk.deb -y
+# wget -O splunk.deb "https://download.splunk.com/products/splunk/releases/9.4.1/linux/splunk-9.4.1-e3bdab203ac8-linux-amd64.deb"
+# sudo dpkg -i splunk.deb -y
 
 
