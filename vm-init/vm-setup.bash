621377c9d8d114b12bf7e9949ea5dcbffa91e3f8
David Siemienas <david@siemienas.dev>
2025-04-08 20:40:10 +0000

    Dun

---
+++ vm-init/vm-setup.bash
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "
+
+Doing cert and proxy stuff
+
+"
+
+sudo cp proxy.crt /usr/share/ca-certificates/.
+echo proxy.crt | sudo tee -a /etc/ca-certificates.conf >/dev/null
+sudo update-ca-certificates
+
+echo "http_proxy=http://10.120.0.200:8080/
+https_proxy=http://10.120.0.200:8080/
+no_proxy=10.0.0.0/8" >>~/.bashrc
+
+source .bashrc
+
+echo "use_proxy=yes
+http_proxy=http://10.120.0.200:8080/
+https_proxy=http://10.120.0.200:8080/
+no_proxy=10.0.0.0/8" >>.wgetrc
+
+# Update and upgrade the system
+sudo apt-get update
+# sudo apt-get upgrade -y
+
+# Install the necessary packages
+sudo apt-get install -y \
+  curl \
+  wget \
+  ca-certificates
+
+# Install Docker and Docker Compose
+# Copied instructions taken from https://docs.docker.com/engine/install/ubuntu/
+
+echo "
+
+
+Prep for Docker install
+
+
+"
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+wget http://download.docker.com/linux/ubuntu/gpg
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg
+sudo mv gpg /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+echo "
+
+Install Docker
+
+"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo mkdir -p /etc/docker/certs.d/registry-1.docker.io
+sudo cp proxy.crt /etc/docker/certs.d/registry-1.docker.io/ca.crt
+
+# Configure Docker to use a proxy
+echo '{
+  "proxies": {
+    "http-proxy": "http://10.120.0.200:8080/",
+    "https-proxy": "http://10.120.0.200:8080/",
+    "no-proxy": "*.test.example.com,.example.org,127.0.0.0/8"
+  }
+}' | sudo tee /etc/docker/daemon.json >/dev/null
+
+# Restart Docker to apply the changes
+sudo systemctl restart docker
+
+# Install Splunk
+#
+# wget -O splunk.deb "https://download.splunk.com/products/splunk/releases/9.4.1/linux/splunk-9.4.1-e3bdab203ac8-linux-amd64.deb"
+# sudo dpkg -i splunk.deb -y
