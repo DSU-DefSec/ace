081f2ca532a114105271a283f354783a9a831ce9
Will Tilstra <152460647+WillTilstra@users.noreply.github.com>
2025-04-11 14:10:05 -0500

    Update psswd.ps1

--- windows/psswd.ps1
+++ windows/psswd.ps1
@@ -11,24 +11,34 @@ function Get-Password {
 $LocalUsers = Get-LocalUser | Where-Object { $_.Enabled -eq $true -and $_.Name -notin @('Guest', 'Deafault Account', 'WDAGUtilityAccount') }
 
   foreach($User in $LocalUsers){
+    $q=Get-Password
+    $l=Get-Password
     $p = Get-Password
     try{
       Set-LocalUser -Name $User.Name -Password (ConvertTo-SecureString $p -AsPlainText -Force)
+      Set-LocalUser -Name $User.Name -Password (ConvertTo-SecureString $q -AsPlainText -Force)
+      Set-LocalUser -Name $User.Name -Password (ConvertTo-SecureString $l -AsPlainText -Force)
+
       Write-Host "Password changed for $($User.Name)" -ForegroundColor Green
-      Write-Host $p
+      Write-Host $l
     } catch {
       Write-Host "Password change failed for $($User.Name)" -ForegroundColor Red
     }
   }
 
-$DomainUsers = Get-ADUser -Filter * | Where-Object { $_.SamAccountName -notmatch '\$$' -and $_.SamAccountName -ne 'Administrator' }
+$DomainUsers = Get-ADUser -Filter * | Where-Object { $_.SamAccountName -notmatch '\$$'}
 
 foreach ($User in $DomainUsers) {
   $p = Get-Password
+  $q=Get-Password
+  $l=Get-Password
   try {
       Set-ADAccountPassword -Identity $User.SamAccountName -NewPassword (ConvertTo-SecureString $p -AsPlainText -Force) -reset
+      Set-ADAccountPassword -Identity $User.SamAccountName -NewPassword (ConvertTo-SecureString $q -AsPlainText -Force) -reset
+      Set-ADAccountPassword -Identity $User.SamAccountName -NewPassword (ConvertTo-SecureString $l -AsPlainText -Force) -reset
+
       Write-Host "Domain password changed for $($User.SamAccountName)" -ForegroundColor Green
-      Write-Host $p
+      Write-Host $l
   } catch {
       Write-Host "Failed to change domain password for $($User.SamAccountName): $_" -ForegroundColor Red
   }
